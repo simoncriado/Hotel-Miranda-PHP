69e187c5752c8b8af51c76e3cf5cbb39e5368eab
Simon Criado <50947741+simoncriado@users.noreply.github.com>
2023-02-20 17:53:42 +0100

    project setup

---
+++ views/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @section('head')
+        @parent
+    @endsection
+</head>
+
+<body>
+    @section('topbar')
+        @parent
+    @endsection
+    </body>
+
+</html>
---
+++ views/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @section('head')
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+        <link href="/src/css/styles.css" rel="stylesheet">
+        <title>Hotel Miranda</title>
+    @show
+</head>
+
+<body>
+@section('topbar')
+        <h1>Working?</h1>
+    @show
+
+    @section('content')
+    @show
+</body>
+
+</html>
